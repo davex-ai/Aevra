05dc299d178d9e2ec9e2ade723168630418faf86
davex-ai <omotoyedave4@gmail.com>
2026-01-28 13:39:11 +0100

    firebase

---
+++ lib/auth.ts
@@ -0,0 +1,18 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+
+const register = async (email: string, password: string) => {
+  const userCredential =
+    await createUserWithEmailAndPassword(auth, email, password);
+
+  return userCredential.user;
+};
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const login = async (email: string, password: string) => {
+  const userCredential =
+    await signInWithEmailAndPassword(auth, email, password);
+
+  return userCredential.user;
+};
